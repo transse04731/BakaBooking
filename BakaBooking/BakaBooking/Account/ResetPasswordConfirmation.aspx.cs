440afd8577856c612329ffc54d520e350ef73f6e
transse04731 <triracle@gmail.com>
2017-06-22 22:14:09 +0700

    First commit

---
+++ BakaBooking/BakaBooking/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace BakaBooking.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ BakaBooking/BakaBooking/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BakaBooking.Startup))]
+namespace BakaBooking
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
